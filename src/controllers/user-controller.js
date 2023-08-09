740dc1f4bc2ce964dbef8e0928090eeda89aa6f9
leander006 <leanderdsilva06@gmail.com>
2023-08-09 21:00:48 +0530

    Added basic user signup routes

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,29 @@
+const UserService = require("../services/user-service");
+
+const userService = new UserService();
+
+const create = async (req, res) => {
+  try {
+    const user = await userService.create({
+      email: req.body.email,
+      password: email.body.password,
+    });
+    return res.status(201).json({
+      success: true,
+      data: user,
+      message: "Successfully created a new user",
+      err: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      data: {},
+      message: "Something went wrong",
+      err: error,
+    });
+  }
+};
+
+module.exports = {
+  create,
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
 
 const app = express();
 
-app.listen(PORT, (req, res) => {
-  console.log(`Server started on port ${PORT}`);
-});
+const prepareAndStartServer = () => {
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, (req, res) => {
+    console.log(`Server started on port ${PORT}`);
+  });
+};
+
+prepareAndStartServer();
